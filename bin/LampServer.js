8242a907e68895e32999b86dbd56c41c916474f0
Jingrui Li <1079805974@qq.com>
2018-07-28 23:06:19 +0800

    aaa

--- bin/LampServer.js
+++ bin/LampServer.js
@@ -1,22 +1,27 @@
 var net = require('net')
-const webSocketServer = require('./WebSocket');
+const webs = require('./WebSocket');
 
 var HOST = '0.0.0.0'
 var PORT = 10086
-var nodemcuSocket = null, nodemcuSocket2 = null
+var socket1,socket2
 net.createServer(function (socket) {
-    if(nodemcuSocket == null){
-        nodemcuSocket = socket
+    if(socket1 == null){
+        socket1 = socket
         console.log('第一个NodeMCU连接!')
-        nodemcuSocket.on('data', function (data) {
+        //当网页来消息时候发给第一个nodemcu
+        webs.addMessageListener(function(message){
+            socket.write(message)
+        })
+        socket.on('data', function (data) {
             //第一个NodeMCU的消息传送给每个打开的网页.
-            webSocketServer.broadcast(data)
+            webs.broadcast(data.toString())
         });
+
     }else{
         console.log('第二个NodeMCU连接!')
-        nodemcuSocket2 = socket
-        nodemcuSocket2.on('data', function (data) {
-            
+        socket2 = socket
+        socket.on('data', function (data) {
+                
         });
     }
     console.log('CONNECTED: ' +
@@ -30,5 +35,3 @@ net.createServer(function (socket) {
     });
 
 }).listen(PORT, HOST);
-
-module.exports = {nodemcuSocket, nodemcuSocket2}
--- bin/WebSocket.js
+++ bin/WebSocket.js
@@ -1,27 +1,40 @@
-const {nodemcuSocket, nodemcuSocket2} = require('./LampServer')
 var webSocketServer = require('socket.io')(8080);
+var webs = {
+    clients: [],
+    callback: [],
+    push(val){
+        this.clients.push(val)
+    },
+    broadcast(message){
+        this.clients.forEach(client => {
+            client.send(message)
+        })
+    },
+    addMessageListener(func){
+        this.callback.push(func)
+        console.log(this.callback)
+    },
+    emitOnMessage(message){
+        this.callback.forEach(
+            ele => ele(message)
+        )
+    }
+}
 
 webSocketServer.on('connection', function (socket) {
+    webs.push(socket)
     console.log('connect!')
     socket.on('message', function incoming(message) {
-        console.log(new Date().toLocaleString()+' 网页传来消息: '+message)
-        //把传来的消息发给第一个NodeMCU
-        if(nodemcuSocket!=null)
-            nodemcuSocket.write(message)
-
+        console.log(new Date().toLocaleString() + ' 网页传来消息: ' + message)
+        //触发来消息事件, 不用看懂...
+        //只要知道在LampServer中怎么用就行了
+        webs.emitOnMessage(message)
         //回传消息
         socket.send('收到你的消息啦!!!')
     });
-  socket.on('disconnect', function () { });
+    socket.on('disconnect', function () { });
 });
-
-webSocketServer.broadcast = data =>
-    webSocketServer.clients.forEach(client =>{
-            if (client.readyState === WebSocket.OPEN)
-                client.send(data)
-        }
-)
 
 console.log('服务器启动成功')
 
-module.exports = webSocketServer
+module.exports = webs
